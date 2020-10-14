77571c81b5cea39921cf8eb81d2cd49c6b6aa58a
grant-norris <grant.norris01@stclairconnect.ca>
2020-10-14 11:04:47 -0400

    Assignment 2

--- AngularAssignment1/src/app/app.module.ts
+++ AngularAssignment1/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
+import { ContentListComponent } from './content-list/content-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentCardComponent,
+    ContentListComponent,
   ],
   imports: [
     BrowserModule
--- AngularAssignment1/src/app/content-card/content-card.component.ts
+++ AngularAssignment1/src/app/content-card/content-card.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ContentList } from '../helper-files/ContentList';
+import { Component, Input , OnInit } from '@angular/core';
 import { Content} from '../helper-files/content-interfaces';
+
 
 @Component({
   selector: 'app-content-card',
@@ -8,13 +8,10 @@ import { Content} from '../helper-files/content-interfaces';
   styleUrls: ['./content-card.component.css']
 })
 export class ContentCardComponent implements OnInit {
-content: ContentList;
-item1: Content = {author: 'grant', body: 'thats me', id: 0, title: 'the chosen one'};
-item2: Content = {author: 'dylan', body: 'best friend ', id: 1, title: 'the normal one'};
-item3: Content = {author: 'ben', body: 'other friend', id: 2, title: 'the salty one'};
+  @Input() content: Content;
 
 
-  constructor() {this.content.add(this.item1); this.content.add(this.item2); this.content.add(this.item3); }
+  constructor() { }
 
   ngOnInit(): void {
   }
