177f9f6fbb601bec4c1fc99bcacccce64c501f2d
grant-norris <grant.norris01@stclairconnect.ca>
2020-10-21 02:16:21 -0400

    Assignment 3

--- AngularAssignment1/src/app/app.module.ts
+++ AngularAssignment1/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
+import { ContentTypePipePipe } from './content-type-pipe.pipe';
 
 
 @NgModule({
@@ -11,6 +12,7 @@ import { ContentListComponent } from './content-list/content-list.component';
     AppComponent,
     ContentCardComponent,
     ContentListComponent,
+    ContentTypePipePipe,
   ],
   imports: [
     BrowserModule
