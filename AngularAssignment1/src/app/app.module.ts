ad3ce343560df3f89248870dd5e665a353389178
grant-norris <grant.norris01@stclairconnect.ca>
2020-11-27 21:28:48 -0500

    Assignment 7

--- AngularAssignment1/src/app/app.module.ts
+++ AngularAssignment1/src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { InMemoryDataService} from './in-memory-data.service';
 import { AppComponent } from './app.component';
 import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { ContentTypePipePipe } from './content-type-pipe.pipe';
 import { HoverStyleDirective } from './hover-style.directive';
 import { MessagesComponent } from './messages/messages.component';
+import { CreateComponentComponent } from './create-component/create-component.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 
 @NgModule({
@@ -17,11 +21,20 @@ import { MessagesComponent } from './messages/messages.component';
     ContentTypePipePipe,
     HoverStyleDirective,
     MessagesComponent,
+    CreateComponentComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {
+        dataEncapsulation: false,
+        delay: 1000
+      }),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- AngularAssignment1/src/app/helper-files/content.service.ts
+++ AngularAssignment1/src/app/helper-files/content.service.ts
@@ -3,15 +3,29 @@ import { Content } from './content-interfaces';
 import {CONTENTList} from '../contentDB';
 import { MessageService } from '../message.service';
 import {Observable, of} from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
-
-  constructor(private messageService: MessageService) { }
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-type':
+        'application/json' })
+  };
+  constructor(private messageService: MessageService, private http: HttpClient) { }
   getContentObs(): Observable<Content[]>{ // asynchronous
     this.messageService.add('Content: fetched Content');
     return of(CONTENTList);
+  }
+  getContent(): Observable<Content[]>{
+    return this.http.get<Content[]>('api/content');
+  }
+  addContent(content: Content): Observable<Content>{
+    return this.http.post<Content>('api/content', content,
+      this.httpOptions);
+  }
+  updateGame(content: Content): Observable<any>{
+    return this.http.put<Content>('api/content', content, this.httpOptions);
   }
 }
